06984010fd3b2af502922c31297c4cb1655e8937
jay1coding <jay1coding@gmail.com>
2022-07-14 19:48:18 +0900

    SocketIO is Amazing

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector('input');
+  // 3번째 인자를 서버로 넘겨서 콜백함
+  socket.emit('enter_room', { payload: input.value }, () => {
+    console.log('server is done!');
+  });
+  input.value = '';
+}
+
+form.addEventListener('submit', handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -9,15 +9,20 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-
 app.get('/', (_, res) => res.render('home'));
-app.get('*', (_, res) => res.redirect('/'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  console.log(socket);
+  // 클라이언트의 emit 3번째 인자를 받아 done을 콜백으로 실행.... 세상에
+  socket.on('enter_room', (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 // 같은 서버에서 http, WebSockets를 둘다 작동시킴
