91eb5ec8e75ba041a5f0ffbbcd8f1c6ec4c8cf0c
jay1coding <jay1coding@gmail.com>
2022-07-15 18:04:20 +0900

    User Video

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 // import WebSocket from 'ws';
 // import { send } from 'process';
 import http from 'http';
-import { Server } from 'socket.io';
+import SocketIO from 'socket.io';
 import express from 'express';
-const { instrument } = require('@socket.io/admin-ui');
 
 const app = express();
 
@@ -14,61 +13,7 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ['https://admin.socket.io'],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, {
-  auth: false,
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on('connection', (socket) => {
-  socket['nickname'] = 'Anon';
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('disconnecting', () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
-    );
-  });
-  socket.on('disconnect', () => {
-    wsServer.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('new_message', (msg, room, done) => {
-    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
-});
+const wsServer = SocketIO(httpServer);
 
 // 같은 서버에서 http, WebSockets를 둘다 작동시킴
 // const server = http.createServer(app);
