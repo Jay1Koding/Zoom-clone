6051f692fa2b76bb1ff9bb4f318d05dbba1f2b12
jay1coding <jay1coding@gmail.com>
2022-07-16 17:19:00 +0900

    Answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -93,17 +93,18 @@ camerasSelect.addEventListener('input', handleCameraChange);
 const welcome = document.getElementById('welcome');
 const welcomeForm = welcome.querySelector('form');
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector('input');
-  socket.emit('join_room', input.value, startMedia);
+  await initCall();
+  socket.emit('join_room', input.value);
   roomName = input.value;
   input.value = '';
 }
@@ -112,7 +113,6 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 
 // Socket Code
 
-// A side
 socket.on('welcome', async () => {
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
@@ -120,9 +120,15 @@ socket.on('welcome', async () => {
   socket.emit('offer', offer, roomName);
 });
 
-// B side
-socket.on('offer', (offer) => {
-  console.log(offer);
+socket.on('offer', async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+});
+
+socket.on('answer', (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 // RTC Code
